dc702540a629ee9905fb7db1b50a1088757da9a2
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-12-15 09:37:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+A salty node package to quickly generate salted hashes for system uuids.
+
+## Install
+
+![npm](https://img.shields.io/npm/v/salty-system?style=flat&color=blue)
+![NPM](https://img.shields.io/npm/l/salty-system?style=flat&color=blue)
+
+```bash
+yarn global add salty-system
+```
+
+```bash
+npm install -g salty-system
+```
+
+## Upgrade
+
+![npms.io (maintenance)](https://img.shields.io/npms-io/maintenance-score/salty-system?style=flat&color=blue)
+![GitHub last commit](https://img.shields.io/github/last-commit/grimmbraten/salty-system?style=flat&color=blue)
+![GitHub commits since latest release (by date)](https://img.shields.io/github/commits-since/grimmbraten/salty-system/latest/master?style=flat&color=blue)
+
+```bash
+yarn global upgrade salty-system --latest
+```
+
+```bash
+npm update -g salty-system
+```
+
+## Uninstall
+
+```bash
+yarn global remove salty-system
+```
+
+```bash
+npm uninstall -g salty-system
+```
+
+![GitHub Repo stars](https://img.shields.io/github/stars/grimmbraten/salty-system?style=social)
+![npm](https://img.shields.io/npm/dt/salty-system?logo=npm&style=social)
---
+++ src/main.js
@@ -0,0 +1,36 @@
+const ora = require('ora');
+const clipboard = require('clipboardy');
+const { hashSync, genSaltSync } = require('bcryptjs');
+
+const obj = {};
+let copy = false;
+const [, , ...args] = process.argv;
+
+const spinner = ora('Generating salt').start();
+
+if (args.length === 0) spinner.fail('No arguments provided');
+else if (args.length === 1)
+  if (args[0].length === 36) {
+    clipboard.writeSync(hashSync(args[0], genSaltSync(10)));
+    spinner.succeed('Salt copied to clipboard');
+  } else spinner.fail('Invalid argument');
+else {
+  args.map((arg, index) => {
+    if (arg === '--id') obj.id = args[index + 1];
+    else if (arg === '--name') obj.name = args[index + 1];
+    else if (arg === '--email') obj.email = args[index + 1];
+    else if (arg === '--copy') copy = true;
+  });
+
+  if (obj.id.length === 36) {
+    obj.hash = hashSync(obj.id, genSaltSync(10));
+
+    if (copy) {
+      clipboard.writeSync(Object.values(obj).join(', '));
+      spinner.succeed('Salt copied to clipboard');
+    } else {
+      spinner.succeed('Salt printed to console\n');
+      console.log(obj);
+    }
+  } else spinner.fail('Invalid argument');
+}
